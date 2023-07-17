958d6b2c7eb00783c69006c104b95e32ad44c012
Randy <weton0110@gmail.com>
2023-07-18 00:44:58 +0800

    feat Entity Framework

---
+++ Pazzo/Pazzo/Models/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pazzo.Models.Repositories
+{
+    public interface IRepository<Table>
+    {
+        void Create(Table _entity);
+
+        void Delete(int id);
+
+        IEnumerable<Table> GetAll();
+
+        Table GetByID(int id);
+
+        void Update(Table _entity);
+    }
+}
---
+++ Pazzo/Pazzo/Models/Repositories/PazzoRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Pazzo.Models.Repositories
+{
+    public class PazzoRepository<Table> : IRepository<Table> where Table : class
+    {
+        private pazzoEntities db = null;
+        private DbSet<Table> dbTable = null;
+
+        public PazzoRepository()
+        {
+            db = new pazzoEntities();
+            dbTable = db.Set<Table>();
+        }
+
+        public void Create(Table _entity)
+        {
+            dbTable.Add(_entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            dbTable.Remove(GetByID(id));
+            db.SaveChanges();
+        }
+
+        public IEnumerable<Table> GetAll()
+        {
+            return dbTable;
+        }
+
+        public Table GetByID(int id)
+        {
+            return dbTable.Find(id);
+        }
+
+        public void Update(Table _entity)
+        {
+            db.Entry<Table>(_entity).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
